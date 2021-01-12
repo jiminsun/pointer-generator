9c8d81c3ecdd1cf6bd8a94ceaf93b6b3c559f00a
jiminsun <jiminxsun@gmail.com>
2021-01-12 14:05:00 +0900

    fixed gpu setting error, removed mds dataset related options

--- train.py
+++ train.py
@@ -13,30 +13,34 @@ from utils import *
 
 
 def main(config):
-    # setup GPU device if available
-    device, device_ids = prepare_device(config['n_gpu'])
-
     # fix random seeds for reproducibility
     SEED = 123
     pl.seed_everything(SEED)
 
+    # generate experiment name
+    exp_name = generate_exp_name(config)
+
     # Train
-    # 1. Load training dataset and build / load vocab
-    if os.path.exists(config.path.vocab):
-        train_data, vocab = build_dataset(
-            data_path=config.path.train,
-            load_vocab=config.path.vocab,
-            config=config,
-            is_train=True
-        )
+    # 1. Load dataset, build or load vocab file
+    # if use pre-built vocab
+    if os.path.exists(config.path.vocab) and config.load_vocab:
+        load_vocab = config.path.vocab
     else:
-        train_data, vocab = build_dataset(
-            data_path=config.path.train,
-            load_vocab=None,
-            config=config,
-            is_train=True
-        )
-        vocab.save(config.path.vocab)
+        load_vocab = None
+
+    # 1-1. Training dataset
+    # If validation set is not specified, take 10% of training data as validation set
+    split_dev = 0.1 if config.path.val == '' else 0.0
+    (train_data, val_data), vocab = build_dataset(
+        data_path=config.path.train,
+        load_vocab=load_vocab,
+        config=config,
+        is_train=True,
+        split_dev=split_dev,
+    )
+
+    # save vocab file
+    vocab.save(os.path.join(f'logs/{exp_name}', config.path.vocab))
 
     train_loader = build_dataloader(
         dataset=train_data,
@@ -47,13 +51,15 @@ def main(config):
         num_workers=config.data_loader.num_workers,
     )
 
-    # 2. Load validation dataset
-    val_data = build_dataset(
-        data_path=config.path.val,
-        config=config,
-        is_train=False,
-        vocab=vocab
-    )
+    # 1-2. Load validation dataset
+    # For validation, we use NIKL
+    if val_data is None:
+        val_data = build_dataset(
+            data_path=config.path.val,
+            config=config,
+            is_train=False,
+            vocab=vocab
+        )
 
     val_loader = build_dataloader(
         dataset=val_data,
@@ -64,27 +70,66 @@ def main(config):
         num_workers=config.data_loader.num_workers,
     )
 
-    # 3. Build model instance
+    # 2. Build model instance
+
     model = SummarizationModel(
         config=config,
         vocab=vocab,
     )
 
-    # 4. Set logger, trainer
-    tb_logger = pl_loggers.TensorBoardLogger('logs/', version=1)
+    # 3. Set logger, trainer
+    tb_logger = pl_loggers.TensorBoardLogger(
+        save_dir='logs/',
+        name=exp_name,
+    )
+    tb_logger.log_hyperparams(config)
 
-    trainer = pl.Trainer(
-        logger=tb_logger,
-        default_root_dir='./checkpoints/',
-        gpus=1,
-        max_epochs=config.trainer.epochs,
+    if config.stop_with == 'loss':
+        monitor = 'val_loss_avg'
+        mode = 'min'
+    else:
+        which_rouge = config.stop_with[-1]  # 1, 2 or l
+        monitor = f'rouge_{which_rouge}_avg'
+        mode = 'max'
+
+    filepath = '{epoch}-{' + monitor + ':.2f}'
+    checkpoint_callback = ModelCheckpoint(
+        filepath=os.path.join(f'logs/{exp_name}', filepath),
+        verbose=False,
+        monitor=monitor,
+        mode=mode,
+        save_top_k=5,
     )
 
-    # 5. Train!
+    # early stopping
+    early_stop_callback = EarlyStopping(
+        monitor=monitor,
+        min_delta=0.00,
+        patience=10,
+        verbose=True,
+        mode=mode,
+    )
+
+    if config.device == -1:
+        gpus = None
+    else:
+        gpus = [config.device]
+        
+    trainer = pl.Trainer(
+        logger=tb_logger,
+        callbacks=[early_stop_callback],
+        gpus=gpus,
+        resume_from_checkpoint=config.model_path,
+        max_epochs=config.trainer.epochs,
+        checkpoint_callback=checkpoint_callback,
+        gradient_clip_val=config.trainer.max_grad_norm,
+        log_every_n_steps=500,
+    )
+
+    # 4. Train!
     trainer.fit(model, train_loader, val_loader)
 
-    # 6. Evaluation
-    vocab = Vocab.from_json(config.path.vocab)
+    # 5. Evaluation
     test_data = build_dataset(
         data_path=config.path.test,
         config=config,
@@ -95,14 +140,17 @@ def main(config):
     test_loader = build_dataloader(
         dataset=test_data,
         vocab=vocab,
-        batch_size=config.data_loader.batch_size.test,
+        batch_size=1,
         max_decode=config.data.tgt_max_test,
         is_train=False,
         num_workers=config.data_loader.num_workers,
     )
 
     test_outputs = trainer.test(model, test_loader)
-    print(test_outputs[0:3])
+    output_name = generate_output_name(config)
+    write_output(test_loader=test_loader,
+                 test_outputs=test_outputs,
+                 fname=os.path.join(f'logs/{exp_name}', output_name))
 
 
 if __name__ == "__main__":
@@ -112,6 +160,56 @@ if __name__ == "__main__":
         default='config.json',
         type=str,
         help='path to config file'
+    )
+
+    args.add_argument(
+        '--mds',
+        default=None,
+        type=str,
+        help='multi-news labeling method to employ. if None, nikl dataset is used.'
+    )
+
+    args.add_argument(
+        '-m', '--model-path',
+        default=None,
+        type=str,
+        help='path to load model'
+    )
+
+    args.add_argument(
+        '--load-vocab',
+        action='store_true',
+        default=False,
+        help='whether to load pre-built vocab file'
+    )
+
+    args.add_argument(
+        '--stop-with',
+        default='rl',
+        type=str,
+        choices=['loss', 'r1', 'r2', 'rl'],
+        help='validation evaluation metric to perform early stopping'
+    )
+
+    args.add_argument(
+        '-e', '--exp-name',
+        default='',
+        type=str,
+        help='suffix to specify experiment name'
+    )
+
+    args.add_argument(
+        '-d', '--device',
+        default=-1,
+        type=int,
+        help='gpu device number to use. if cpu, set this argument to -1'
+    )
+
+    args.add_argument(
+        '-n', '--note',
+        default='',
+        type=str,
+        help='note to append to result output file name'
     )
 
     sys.path.append(
